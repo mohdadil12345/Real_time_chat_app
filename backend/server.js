34cb473f0fddcd478446b591c31411eed2de7750
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-05-05 02:29:02 +0530

    signup done

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config()
 
 import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./db/connectToMongoDb.js";
+import cookieParser from "cookie-parser";
 
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-dotenv.config()
 
 app.use(express.json())
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 
