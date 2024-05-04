8d328a0f9b1ee57506281979f703f4aa717fe682
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-05-05 01:57:30 +0530

    backedn npm

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+
+
+export const signup = async (req, res) => {
+  try {
+
+    const {fullName, username, password, confirmPassword, gender} = req.body
+    
+  } catch (error) {
+    
+  }
+
+}
+
+
+
+export const login = async (req, res) => {
+    console.log("login user")
+
+
+}
+
+
+export const logout = (req, res) => {
+    console.log("logout user")
+
+}
